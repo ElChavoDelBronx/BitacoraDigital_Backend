888bbfca106c22f10ab16d307bbd64f16a0ceaea
XxDamian-hc2006xX <hdamian955@gmail.com>
2026-03-10 22:42:50 -0600

    Añado modulo period repository, service, controller y modifico el entity

--- src/main/java/mx/edu/utez/bitacoradigitalservices/modules/period/Period.java
+++ src/main/java/mx/edu/utez/bitacoradigitalservices/modules/period/Period.java
@@ -6,10 +6,9 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import mx.edu.utez.bitacoradigitalservices.kernel.BaseEntity;
 import mx.edu.utez.bitacoradigitalservices.modules.projects.Project;
-import org.springframework.cglib.core.Local;
+
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -26,4 +25,46 @@ public class Period extends BaseEntity {
 
     @OneToMany(mappedBy = "period")
     private List<Project> projects;
+
+    public Period() {}
+
+    public String getNamePeriod() {
+        return namePeriod;
+    }
+
+    public void setNamePeriod(String namePeriod) {
+        this.namePeriod = namePeriod;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
 }
